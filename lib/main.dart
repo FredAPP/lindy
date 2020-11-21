ede79146b9e3928be86d4ff86eda33c29da3fa59
FredericoPenido <docreifer@gmail.com>
2020-11-21 09:57:21 -0300

    tteste2

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter'),
     );
   }
 }
