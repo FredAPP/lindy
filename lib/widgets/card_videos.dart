d5fec50f43c6e9bf03356dff6b702933fc9a0486
Wívio Lima <wivio.lima@gmail.com>
2021-01-19 22:10:07 -0300

    Refactor moves_page

--- lib/widgets/card_videos.dart
+++ lib/widgets/card_videos.dart
@@ -6,7 +6,7 @@ class CardVideos extends StatelessWidget {
   YoutubePlayerController _controller = YoutubePlayerController(
     initialVideoId: 'iLnmTe5Q2Qw',
     flags: YoutubePlayerFlags(
-      autoPlay: true,
+      autoPlay: false,
       mute: true,
     ),
   );
@@ -14,36 +14,46 @@ class CardVideos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.09,
-            height: MediaQuery.of(context).size.height * 0.009,
-          ),
-          Card(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.90,
-                  height: MediaQuery.of(context).size.height * 0.30,
-                  color: Colors.blue[600],
-                  child: YoutubePlayer(
-                    controller: _controller,
-                    showVideoProgressIndicator: true,
-                  ),
-                ),
-                ListTile(
-                  title: Text(
-                    'Video Title',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  leading: Icon(Icons.album),
-                  trailing: Icon(Icons.table_chart),
-                )
-              ],
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(232, 213, 173, 1),
+          // border: Border.all(
+          //   color: Color.fromRGBO(60, 65, 133, 1),
+          //   width: 10,
+        ),
+        // borderRadius: BorderRadius.all(Radius.circular(5)),
+
+        child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.09,
+              height: MediaQuery.of(context).size.height * 0.009,
             ),
-          ),
-        ],
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    height: MediaQuery.of(context).size.height * 0.30,
+                    color: Colors.blue[600],
+                    child: YoutubePlayer(
+                      controller: _controller,
+                      showVideoProgressIndicator: true,
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Video Title',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    leading: Icon(Icons.album),
+                    trailing: Icon(Icons.table_chart),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/dropdown.dart
+++ lib/widgets/dropdown.dart
@@ -14,32 +14,55 @@ class _DropdownMenuState extends State<DropdownMenu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Dropdown Menu'),
-        ),
-        body: Column(children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: DropdownButton(
-              hint: Text('Select Difficulty'),
-              icon: Icon(Icons.arrow_drop_down),
-              iconSize: 36,
-              isExpanded: true,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
+      // appBar: AppBar(
+      //   title: Text('Dropdown Menu'),
+      // ),
+      body: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(232, 213, 173, 1)),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(232, 213, 173, 1),
+
+                // borderRadius: BorderRadius.all(Radius.circular(5)),
               ),
-              value: valorEscolhido,
-              onChanged: (novoValor) {
-                setState(() {
-                  valorEscolhido = novoValor;
-                });
-              },
-              items: lista.map((item) {
-                return DropdownMenuItem(value: item, child: Text(item));
-              }).toList(),
-            ),
-          )
-        ]));
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Theme(
+                  data: Theme.of(context).copyWith(
+                    canvasColor: Color.fromRGBO(232, 213, 173, 1),
+                  ),
+                  child: ButtonTheme(
+                    splashColor: Color.fromRGBO(232, 213, 173, 1),
+                    alignedDropdown: true,
+                    buttonColor: Color.fromRGBO(232, 213, 173, 1),
+                    child: DropdownButton(
+                      hint: Text('Select Difficulty'),
+                      icon: Icon(Icons.arrow_drop_down),
+                      iconSize: 36,
+                      isExpanded: true,
+                      style: TextStyle(
+                        color: Color.fromRGBO(60, 65, 133, 1),
+                        fontSize: 16,
+                      ),
+                      value: valorEscolhido,
+                      onChanged: (novoValor) {
+                        setState(() {
+                          valorEscolhido = novoValor;
+                        });
+                      },
+                      items: lista.map((item) {
+                        return DropdownMenuItem(value: item, child: Text(item));
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
