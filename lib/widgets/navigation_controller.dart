14a5c1d4dfdf3092cce140041dce6780300cf563
FredericoPenido <docreifer@gmail.com>
2021-01-31 10:41:43 -0300

    commit com persistent tab bot nav

--- lib/widgets/navigation_controller.dart
+++ lib/widgets/navigation_controller.dart
@@ -1,64 +1,97 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lindyapp/pages/credits_page.dart';
 import 'package:lindyapp/pages/events_page.dart';
 import 'package:lindyapp/pages/home.dart';
 import 'package:lindyapp/pages/moves_page.dart';
 import 'package:lindyapp/pages/playlists_page.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
-class NavigationController extends StatefulWidget {
-  @override
-  _NavigationControllerState createState() => _NavigationControllerState();
-}
-
-class _NavigationControllerState extends State<NavigationController> {
-  int _currentIndex = 0;
-
-  List<Object> paginas = [
-    Home(),
-    MovesPage(),
-    PlaylistsPage(),
-    EventsPage(),
-    CreditsPage(),
-  ];
-  void tapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
+class NavigationController extends StatelessWidget {
+  NavigationController({Key key}) : super(key: key);
+  PersistentTabController _controller;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: paginas[_currentIndex],
+    _controller = PersistentTabController(initialIndex: 0);
+
+    List<Widget> _buildScreens() {
+      return [
+        Home(),
+        MovesPage(),
+        PlaylistsPage(),
+        EventsPage(),
+        CreditsPage(),
+      ];
+    }
+
+    List<PersistentBottomNavBarItem> _navBarsItems() {
+      return [
+        PersistentBottomNavBarItem(
+          icon: Icon(CupertinoIcons.home),
+          title: ("Home"),
+          activeColor: CupertinoColors.activeBlue,
+          inactiveColor: CupertinoColors.systemGrey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: Icon(CupertinoIcons.settings),
+          title: ("Settings"),
+          activeColor: CupertinoColors.activeBlue,
+          inactiveColor: CupertinoColors.systemGrey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: Icon(CupertinoIcons.settings),
+          title: ("Settings"),
+          activeColor: CupertinoColors.activeBlue,
+          inactiveColor: CupertinoColors.systemGrey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: Icon(CupertinoIcons.settings),
+          title: ("Settings"),
+          activeColor: CupertinoColors.activeBlue,
+          inactiveColor: CupertinoColors.systemGrey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: Icon(CupertinoIcons.settings),
+          title: ("Settings"),
+          activeColor: CupertinoColors.activeBlue,
+          inactiveColor: CupertinoColors.systemGrey,
+        ),
+      ];
+    }
+
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white,
+      handleAndroidBackButtonPress: true,
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears.
+      stateManagement: true,
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: tapped,
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu),
-            label: 'Articles',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.directions_run),
-            label: 'Moves',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.playlist_play),
-            label: 'Playlists',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: 'Events',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.supervisor_account),
-            label: 'About us',
-          ),
-        ],
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
       ),
+      screenTransitionAnimation: ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+          NavBarStyle.style8, // Choose the nav bar style with this property.
     );
   }
 }
