9df02fdf3ae02637a0ceecdb1e7be530ffedca94
BitterFields <thomascampos@gmail.com>
2021-03-13 22:01:23 +0000

    Ajustes das bordas da pagina artigos

--- lib/pages/articles_page.dart
+++ lib/pages/articles_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lindyapp/widgets/articles.dart';
 import 'package:lindyapp/widgets/lindyappbar.dart';
 
 class ArticlesPage extends StatelessWidget {
@@ -21,7 +22,7 @@ class ArticlesPage extends StatelessWidget {
               height: heightBody,
               decoration: BoxDecoration(
                 borderRadius:
-                    BorderRadius.circular(5), //Colocar a borda interna
+                    BorderRadius.circular(15), 
                 color: Color.fromRGBO(232, 213, 173, 1),
                 border: Border.all(
                   color: Color.fromRGBO(60, 65, 133, 1),
@@ -32,12 +33,12 @@ class ArticlesPage extends StatelessWidget {
                 height: heightBody,
                 child: ListView(
                   children: <Widget>[
-                    lindyCard(context),
-                    lindyCard(context),
-                    lindyCard(context),
-                    lindyCard(context),
-                    lindyCard(context),
-                    lindyCard(context),
+                    Articles(),
+                    Articles(),
+                    Articles(),
+                    Articles(),
+                    Articles(),
+                    Articles(),
                   ],
                 ),
               ),
@@ -47,77 +48,4 @@ class ArticlesPage extends StatelessWidget {
       ),
     );
   }
-}
-
-Widget lindyCard(context) {
-  //remover context
-  var screenSize = MediaQuery.of(context).size;
-  var height = screenSize.height;
-  var width = screenSize.width;
-  // var heightBody = screenSize.height - 160;
-  return Card(
-    margin: EdgeInsets.symmetric(
-      vertical: 10,
-      horizontal: 15,
-    ),
-    child: Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Row(
-              children: [
-                Image(
-                  image: AssetImage("assets/images/background.jpeg"),
-                  height: height * 0.12,
-                  width: width * 0.20,
-                  fit: BoxFit.fitWidth,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 35),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Swing Dance Baby!",
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromRGBO(60, 65, 133, 1)),
-                      ),
-                      Text(
-                        "Autor: Frankie",
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontStyle: FontStyle.italic,
-                            color: Color.fromRGBO(60, 65, 133, 1)),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Text(
-              "Swing Dance Baby! Swing Dance Baby! Swing Dance Baby! Swing Dance Baby! Swing Dance Baby! Swing Dance Baby! Swing Dance Baby! Swing Dance Baby!",
-              textAlign: TextAlign.justify,
-              style: TextStyle(color: Color.fromRGBO(60, 65, 133, 1)),
-            ),
-          ),
-        ],
-      ),
-    ),
-    color: Color.fromRGBO(232, 213, 173, 1),
-    semanticContainer: true,
-    clipBehavior: Clip.antiAliasWithSaveLayer,
-    shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-        side: BorderSide(
-          color: Color.fromRGBO(60, 65, 133, 1),
-          width: 1,
-        )),
-    elevation: 5,
-  );
 }
--- lib/widgets/articles.dart
+++ lib/widgets/articles.dart
@@ -1,15 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:lindyapp/models/article_info.dart';
 
 class Articles extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: articlesList.length,
-      itemBuilder: (context, index) => ListTile(
-        title: Text(articlesList[index].title),
-        leading: articlesList[index].icon,
+    var screenSize = MediaQuery.of(context).size;
+    var height = screenSize.height;
+    var width = screenSize.width;
+    // var heightBody = screenSize.height - 156;
+    return Card(
+      margin: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 15,
       ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Row(
+                children: [
+                  Image(
+                    image: AssetImage("assets/images/background.jpeg"),
+                    height: height * 0.12,
+                    width: width * 0.20,
+                    fit: BoxFit.fitWidth,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Swing Dance Baby!",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromRGBO(60, 65, 133, 1)),
+                        ),
+                        Text(
+                          "Autor: Frankie",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontStyle: FontStyle.italic,
+                              color: Color.fromRGBO(60, 65, 133, 1)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Text(
+                "Swing Dance Baby! Swing Dance Baby! Swing Dance Baby! Swing Dance Baby! Swing Dance Baby! Swing Dance Baby! Swing Dance Baby! Swing Dance Baby!",
+                textAlign: TextAlign.justify,
+                style: TextStyle(color: Color.fromRGBO(60, 65, 133, 1)),
+              ),
+            ),
+          ],
+        ),
+      ),
+      color: Color.fromRGBO(232, 213, 173, 1),
+      semanticContainer: true,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          side: BorderSide(
+            color: Color.fromRGBO(60, 65, 133, 1),
+            width: 1,
+          )),
+      elevation: 5,
     );
   }
 }
